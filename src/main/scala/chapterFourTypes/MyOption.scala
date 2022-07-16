343287dc926f2111341487a77626ed4fba48890c
TedCadet <e_cadet22@hotmail.com>
2022-07-16 14:47:17 -0400

    modif on MyOption: def isEmpty added,def map2

--- src/main/scala/chapterFourTypes/MyOption.scala
+++ src/main/scala/chapterFourTypes/MyOption.scala
@@ -8,6 +8,17 @@ enum MyOption[+A]:
     case None => None
     case Some(a) => Some(f(a))
 
+  def map2[A, B, C](a:MyOption[A], b: MyOption[B])(f: (A, B) => C): MyOption[C] =
+    (a, b) match
+      case (Some(a: A), Some(b: B)) =>
+        Some(f(a,b))
+      case (Some(None), Some(_)) | (Some(_), Some(None)) =>
+        None
+
+  def isEmpty: Boolean = this match
+    case Some(_) => false
+    case None => true
+
   def flatMap[B](f: A => MyOption[B]): MyOption[B] =
 //    case None => None
 //    case Some(a) => f(a) // works
@@ -16,6 +27,10 @@ enum MyOption[+A]:
   def getOrElse[B >: A](default: => B): B = this match
     case None => default
     case Some(a) => a
+
+//  def get[A](): A = this match
+//    case Some(a) => a
+//    case None => None
 
   def orElse[B >: A](ob: => MyOption[B]): MyOption[B] =
 //    case None => ob // doesn't work
